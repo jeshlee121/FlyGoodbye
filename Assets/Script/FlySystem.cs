b4039985c46527051b0293536723a92941c8c3a1
Jessica Lee <jessicalee@Jessicas-MacBook-Pro-2.local>
2018-06-11 01:02:40 -0700

    VR Decal Project

---
+++ Assets/Script/FlySystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlySystem : MonoBehaviour {
+	public ParticleSystem flysystem;
+	public ParticleSystem splatterParticles;
+	public Text collision;
+	int numCollisions;
+
+	// Use this for initialization
+	void Start () {
+		numCollisions = 0;
+		collision.text = "collisions: 0";
+        //part = GetComponent<ParticleSystem>();
+        //collisionEvents = new List<ParticleCollisionEvent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+//		emits one fly every other frame
+		if (Input.GetKeyDown("j")) {
+			StartCoroutine("SpawnFlies");
+           
+		}
+
+
+	}
+
+	void OnParticleCollision(GameObject other) {
+
+        /*int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Vector3 force = collisionEvents[i].velocity * 10;
+                rb.AddForce(force);
+            }
+            i++;
+        }*/
+        Debug.Log ("collided");
+		EmitAtLocation ();
+		numCollisions += 1;
+		collision.text = "collisions: " + numCollisions.ToString() ; 
+	}
+
+	void EmitAtLocation() {
+		splatterParticles.Emit (1);
+	}
+
+	IEnumerator SpawnFlies() {
+		for (float f = 1f; f >= 0; f -= 0.1f) {
+			flysystem.Emit (1);
+       
+            yield return new WaitForSeconds(.1f);
+		}
+	}
+		
+	
+}
---
+++ Assets/Script/Hand.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    public OVRInput.Controller controller;
+
+    // Use this for initialization
+    private float indexTriggerState = 0;
+    private float handTriggerState = 0;
+    private float oldIndexTriggerState = 0;
+
+    private bool holdingPan = false;
+    private GameObject Pan = null;
+
+    public Vector3 holdPosition = new Vector3(0.0f, -0.025f, 0.03f);
+    public Vector3 holdRotation = new Vector3(0, 180, 0);
+
+    // Update is called once per frame
+    void Update () {
+        oldIndexTriggerState = indexTriggerState;
+        indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+        handTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Pan"))
+        {
+            if (handTriggerState > 0.9f && !holdingPan)
+            {
+               
+                Grab(other.gameObject);
+            }
+        }
+    }
+
+    void Grab(GameObject obj)
+    {
+
+        holdingPan = true;
+        Pan = obj;
+
+        Pan.transform.parent = transform;
+
+        Pan.transform.localPosition = holdPosition;
+        Pan.transform.localEulerAngles = holdRotation;
+
+        Pan.GetComponent<Rigidbody>().useGravity = false;
+        Pan.GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+}
